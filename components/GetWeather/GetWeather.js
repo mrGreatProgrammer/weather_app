682cd631966d73af36e2531b6cf8ef41b30d216b
mrGreateProgrammer <fhgf9048@gmail.com>
2022-11-18 18:52:31 +0500

    web installed

--- components/GetWeather/GetWeather.js
+++ components/GetWeather/GetWeather.js
@@ -1,8 +1,8 @@
 // 40ace7f2f5de9b334f76f48ece232e28
-import axios from "axios";
+// import axios from "axios";
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
-import Loading from "./components/Loading/Loading";
+// import Loading from "./Loading/Loading";
 
 // https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}
 
@@ -10,14 +10,14 @@ export default function GetWeather() {
   const [weatherData, setWeatherData] = React.useState(null);
 
 React.useEffect(()=>{
-  const res = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={40ace7f2f5de9b334f76f48ece232e28}`)
-  console.log(res)
+  // const res = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={40ace7f2f5de9b334f76f48ece232e28}`)
+  // console.log(res)
 }, [])
 
   return (
     <View style={styles.container}>
         
-    <Loading />
+    {/* <Loading /> */}
     </View>
   );
 }
